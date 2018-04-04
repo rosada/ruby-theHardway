d7747c799a9894e51dbebfe2b0800130de65828c
Arif Rosada <arif.rosada@olx.co.id>
2018-04-04 11:40:12 +0700

    next

---
+++ ex29.rb
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+if people < cats
+   puts 'Too many cats! The world is dooomed!'
+end
+
+if people > cats
+   puts 'Too many cats! The world is saved!'
+end
+
+if people < dogs
+   puts 'The world is drooled on!'
+end
+
+if people > dogs
+   puts 'The world is dry'
+end
+
+dogs +=5
+
+if people >= dogs
+   puts "#{dogs} People are greater than or equal to dogs"
+end
+
+if people == dogs
+  puts 'people are dogs'
+end
